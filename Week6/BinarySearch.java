23270355fb7b259191691cfd767c4c3e6dae3226
Seth Howells <66216678+sthowells@users.noreply.github.com>
2021-08-11 22:22:40 -0400

    Create BinarySearch.java

---
+++ Week6/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+	private static int [] list = new int [] {1, 14, 15, 24, 55, 59, 73, 90, 94, 99};	
+	
+    public static void main(String[] args) {
+
+    	// print found once search finds target
+        if (binSearch(list, 73)) {
+            System.out.println("Found 73"); {
+
+            }
+        }
+    }
+
+
+    public static boolean binSearch(final int[] array, final int target) {
+
+        int left = 0; // first in list
+        int right = array.length - 1; // last in list
+        int middle = (left + right) / 2;
+
+        // initialize binary search to one side
+        while (left <= right) {
+            if (array[middle] < target) {
+                left = middle + 1; // set new "left" if target not found
+            } else if (array[middle] == target) {
+                return true;
+            } else {
+                right = middle - 1; // set new "right" if target not found
+            }
+            middle = (left + right) / 2; // find new middle
+        }
+        return false;
+    }
+}
